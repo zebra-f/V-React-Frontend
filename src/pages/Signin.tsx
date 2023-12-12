c082e2489eb9c354d85e27125be7cd30c11c85df
rafal <zebraf.dev@gmail.com>
2023-12-12 01:23:59 +0100

    feat: draft handle submit

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,4 +1,7 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+import kyClient from "../services/ky";
 
 import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
@@ -13,13 +16,100 @@ import Container from "@mui/material/Container";
 
 import GoogleIcon from "@mui/icons-material/Google";
 
+interface signInData {
+  email: string;
+  password: string;
+}
+async function requestSignIn(data: signInData) {
+  try {
+    const response: any = await kyClient.backendApi.post("token/login/", {
+      json: {
+        email: data.email,
+        password: data.password,
+      },
+    });
+    const responseData = await response.json();
+    return { status: response.status, data: responseData };
+  } catch (error: any) {
+    const response = await error.response;
+    const responseData = await response.json();
+    return { status: response.status, data: responseData };
+  }
+}
+
 function SignIn() {
+  const [emailError, setEmailError] = useState({
+    error: false,
+    errorMessage: "",
+  });
+
+  const [passwordError, setPasswordError] = useState({
+    error: false,
+    errorMessage: "",
+  });
+
   const handleSubmit = (event: any) => {
     event.preventDefault();
+
     const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+    let email = data.get("email");
+    let password = data.get("password");
+
+    if (!email && !password) {
+      setEmailError({
+        error: true,
+        errorMessage: "This field is required.",
+      });
+      setPasswordError({
+        error: true,
+        errorMessage: "This field is required.",
+      });
+      return;
+    }
+
+    if (!email) {
+      setEmailError({
+        error: true,
+        errorMessage: "This field is required.",
+      });
+      return;
+    }
+    email = email.toString().trim();
+    if (email.length < 5 || email.indexOf("@") == -1) {
+      setEmailError({
+        error: true,
+        errorMessage: "Incorrect email address.",
+      });
+      return;
+    }
+    setEmailError({
+      ...emailError,
+      error: false,
+    });
+
+    if (!password) {
+      setPasswordError({
+        error: true,
+        errorMessage: "This field is required.",
+      });
+      return;
+    }
+    password = password.toString();
+    if (password.length < 8) {
+      setPasswordError({
+        error: true,
+        errorMessage:
+          "This password is too short. It must contain at least 8 characters.",
+      });
+      return;
+    }
+    setPasswordError({
+      ...passwordError,
+      error: false,
+    });
+
+    requestSignIn({ email: email, password: password }).then((result) => {
+      return result;
     });
   };
 
@@ -56,6 +146,7 @@ function SignIn() {
             name="email"
             autoComplete="email"
             autoFocus
+            helperText={emailError.error ? emailError.errorMessage : ""}
           />
           <TextField
             margin="normal"
@@ -66,6 +157,7 @@ function SignIn() {
             type="password"
             id="password"
             autoComplete="current-password"
+            helperText={passwordError.error ? passwordError.errorMessage : ""}
           />
           <Button
             type="submit"
